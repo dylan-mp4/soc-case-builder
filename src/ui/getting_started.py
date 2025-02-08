b5bda222b805716286cd1644c384a580d10d5428
dylan-mp4 <dylanbowles6831@hotmail.com>
2025-02-08 13:09:53 +0000

    add GettingStarted dialog to guide users in configuring API keys

---
+++ src/ui/getting_started.py
@@ -0,0 +1,26 @@
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton
+
+class GettingStarted(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Getting Started")
+        self.setFixedWidth(600)
+        
+        layout = QVBoxLayout()
+        
+        info_label = QLabel("Welcome to the SOC Case Builder!\n\n"
+                            "This application helps you manage and build cases for security operations.\n\n"
+                            "To get started, you need to configure API keys for various services:\n"
+                            "- AbuseIPDB: Retrieve your API key from https://www.abuseipdb.com/account\n"
+                            "- VirusTotal: Retrieve your API key from https://www.virustotal.com\n"
+                            "- URLScan: Retrieve your API key from https://urlscan.io/user/\n\n"
+                            "Please go to the Settings tab to enter your API keys and other information."
+                            "\n\n Created by Dylan Bowles")
+        info_label.setWordWrap(True)
+        layout.addWidget(info_label)
+        
+        close_button = QPushButton("Close")
+        close_button.clicked.connect(self.accept)
+        layout.addWidget(close_button)
+        
+        self.setLayout(layout)
