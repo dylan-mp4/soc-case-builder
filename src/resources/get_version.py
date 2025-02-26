bee6f7514d6ac030b7b06e98f4c0546413f2eff4
dylan-mp4 <72610316+dylan-mp4@users.noreply.github.com.>
2025-02-26 12:26:21 +0000

    Add version retrieval to build workflow and create get_version script

---
+++ src/resources/get_version.py
@@ -0,0 +1,12 @@
+import re
+
+def get_version():
+    with open('src/resources/version.py', 'r') as file:
+        content = file.read()
+        version_match = re.search(r"__version__ = ['\"]([^'\"]+)['\"]", content)
+        if version_match:
+            return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+if __name__ == "__main__":
+    print(get_version())
